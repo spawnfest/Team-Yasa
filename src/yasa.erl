243b30ea022765c5175df74545b585bcebbf8c1d
Richard Ramsden <richard@rramsden.ca>
2012-07-06 18:05:12 -0700

    setup main api, mockout calls for now

---
+++ src/yasa.erl
@@ -0,0 +1,11 @@
+-module(yasa).
+-export([set/4, incr/4, get/4]).
+
+set(Schema, Key, Timestamp, Value) ->
+    ok.
+
+incr(Schema, Key, Timestamp, Value) ->
+    ok.
+
+get(Schema, Key, Start, End) ->
+    ok.
