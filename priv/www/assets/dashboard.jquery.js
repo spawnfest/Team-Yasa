36e46c2b495defddebdc0989acd24477b178c16c
Richard Ramsden <richard@rramsden.ca>
2012-07-07 21:33:51 -0700

    along the way

--- priv/www/assets/dashboard.jquery.js
+++ priv/www/assets/dashboard.jquery.js
@@ -1,19 +1,43 @@
 (function($) {
     $.dashboard = {
-        ENDPOINT : "http://localhost:8080/",
-
         init : function() {
             $.ajaxSetup({
                 async: false
             });
 
-            $.getJSON(this.ENDPOINT + "api/keys", function(data) {
+            $.getJSON("/api/keys", function(data) {
                 $.each(data, function(i, key) {
-                    $("#keys").append("<li>" + key + "</li>");
+                    $("#keys").append("<li data-name='"+key+"'><a href='#'>" + key + "</a></li>");
                 });
             });
 
-            console.log(this.ENDPOINT);
+            $('#keys li').click(function() {
+                $.dashboard.graph( $(this).data('name') );
+                return false;
+            });
+        },
+
+        graph : function(key) {
+            var options = {
+                chart: {
+                    renderTo: 'graph',
+                    type: 'area'
+                },
+                title: {
+                    text: key
+                },
+                xAxis: {
+                    type: 'datetime'
+                }
+            };
+
+            $.getJSON("/api/get?range=-5min&key="+key, function(data) {
+                data = $.map(data, function(arr) { return [[arr[0] * 1000, arr[1]]] });
+                console.log(data);
+                
+                options.series = [{name: key, data: data}];
+                new Highcharts.Chart(options);
+            });
         }
     }
 })(jQuery);
